df850dbed0c04097b2ee945e8c290b1ccb8c883d
Andrei Borisov <andrei.borr@gmail.com>
2023-03-22 19:03:32 +0300

    Initial commit

---
+++ src/main/java/by/itacademy/servletapp/FirstServlet.java
@@ -0,0 +1,79 @@
+package by.itacademy.servletapp;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import javax.xml.stream.events.Characters;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.Map;
+
+@WebServlet("/first")
+public class FirstServlet extends HttpServlet {
+    private String msg;
+
+    @Override
+    public void init() throws ServletException {
+        msg = "Привет из FirstServlet";
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset=UTF-8");
+        //resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        resp.setHeader("MYHEADER", "Hi");;
+
+        String param1 = req.getParameter("param1");
+
+        String[] ages = req.getParameterValues("age");
+        Map<String, String[]> parameterMap = req.getParameterMap();
+        try (PrintWriter writer = resp.getWriter();){
+            parameterMap.entrySet().
+                    forEach(e -> writer.println(
+                            "<p>key: " +e.getKey() + " value: " + Arrays.toString(e.getValue()) + "</p>"));
+            writer.println("<h1>ages = " + ages[0] + "</h1>");
+            writer.println("<h1>param1 = " + param1 + "</h1>");
+            writer.println("<br/><a href=\"second\">Открыть Second Servlet</a>");
+        }
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        writer.println("<h2>" + req.getParameter("name") + "</h2>");
+        writer.println("<h2>" + req.getParameter("pwd") + "</h2>");
+
+        writer.println("<form action=\"second\" method=\"POST\">");
+        writer.println("Курс: <input type=\"text\" name=\"course\">");
+        writer.println("<input type=\"submit\" value=\"отправить\">");
+        writer.println("</form>");
+        writer.close();
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/by/itacademy/servletapp/SecondServlet.java
@@ -0,0 +1,34 @@
+package by.itacademy.servletapp;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/second")
+public class SecondServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        try (var writer = resp.getWriter()){
+            writer.println("<h3>Привет из SecondServlet</h3>");
+            writer.println("<br/><a href=\"first\">First Servlet</a>");
+            writer.println("<br/><a href=\"Smth.html\">Go to login</a>");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.sendRedirect("Smth.html");
+        /* resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        writer.println("<h2>" + req.getParameter("course") + "</h2>");
+        writer.close();*/
+
+
+    }
+}
